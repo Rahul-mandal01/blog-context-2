8c88df489a0857977ab66f891abf723988792e6b
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-11 18:15:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import "./App.css";
+import {Routes, Route,useSearchParams, useLocation} from "react-router-dom"
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
-import Header from "./components/Header";
-import Blogs from "./components/Blogs";
-import Pagination from "./components/Pagination";
+import Home from "./Pages/Home";
+import CategoryPage from "./Pages/CategoryPage";
+import BlogPage from "./Pages/BlogPage";
+import TagPage from "./Pages/TagPage";
+
+
+
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const location = useLocation();
+
+
   useEffect(() => {
-    // Fetch the inital Blogposts data
-    fetchBlogPosts();
+
+    const page = searchParams.get("page") ?? 1;
+
+    if(location.pathname.includes("tags")){
+      // iska matlab tag wala page show krna hai
+      const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page), tag);
+
+    }
+
+    else if(location.pathname.includes("categories")){
+      const category = location.pathname.split("/").at(-1).replaceAll("-","");
+      fetchBlogPosts(Number(page), null, category);
+    }
+
+    else{
+      fetchBlogPosts(Number(page));
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [location.pathname, location.search]);
 
   return (
-    <>
-      <Header />
-      <div className="my-[100px]">
-        <Blogs />
-        <Pagination />
-      </div>
-    </>
+
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/blog/:blogId" element={<BlogPage/>} />
+      <Route path="/tags/:tag" element={<TagPage/>} />
+      <Route path="/category/:category" element={<CategoryPage/>} />
+    </Routes>
+
   );
 }
