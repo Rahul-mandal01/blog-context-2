b9f90c40591ed00805239ab485ccabfafd6ca0b9
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-11 18:18:02 +0530

    Create CategoryPage.js

---
+++ src/Pages/CategoryPage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from '../components/Header';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Pagination from '../components/Pagination';
+import Blogs from '../components/Blogs';
+
+const CategoryPage = () => {
+
+  const navigation = useNavigate();
+  const location = useLocation;
+  const category = location.pathname.split("/").at(-1);
+
+  return (
+    <div>
+        <Header/>
+
+        <div>
+          <button onClick={ () => navigation(-1)}>
+            back
+          </button>
+
+          <h2>
+            Blogs On <span>{category}</span>
+          </h2>
+          <Blogs/>
+          <Pagination/>
+        </div>
+
+    </div>
+  )
+}
+
+export default CategoryPage;
