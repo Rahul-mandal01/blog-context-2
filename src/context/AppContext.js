487a624e453ae46234cca595b069428c0fa0280f
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-11 18:17:10 +0530

    Create AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { baseUrl } from "../baseUrl";
 
+
 export const AppContext = createContext();
 
 export default function AppContextProvider({ children }) {
@@ -10,9 +11,19 @@ export default function AppContextProvider({ children }) {
   const [totalPages, setTotalPages] = useState(null);
 
   // Fetch Blog Data
-  const fetchBlogPosts = async (page = 1) => {
+  const fetchBlogPosts = async (page = 1, tag=null, category) => {
     setLoading(true);
     let url = `${baseUrl}?page=${page}`;
+
+    if(tag){
+      url += `&tag=${tag}`;
+    }
+
+    if(category){
+      url += `&category = ${category}`;
+    }
+
+
     try {
       const res = await fetch(url);
       const data = await res.json();
