63ced66a084e5fadb494c1a96a79f324d43644f1
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-13 22:40:12 +0530

    Updated Blogs.js

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -7,19 +7,26 @@ export default function Blogs() {
 
   return (
     <div className="flex flex-col gap-y-10 my-4">
-      {loading ? (
-        <div className="min-h-[80vh] w-full flex justify-center items-center">
-          <p className="text-center font-bold text-3xl">Loading</p>
-        </div>
-      ) : posts.length === 0 ? (
-        <div className="min-h-[80vh] w-full flex justify-center items-center">
-          <p className="text-center font-bold text-3xl">No Blogs Found !</p>
-        </div>
-      ) : (
-        posts.map((post) => (
-          <BlogDetails key={post.id} post={post}/>
-        ))
-      )}
+      {loading ? 
+        (
+          <div className="min-h-[80vh] w-full flex justify-center items-center">
+            <p className="text-center font-bold text-3xl">Loading</p>
+          </div>
+        ) : 
+        (posts.length === 0 ? 
+          (
+            <div className="min-h-[80vh] w-full flex justify-center items-center">
+              <p className="text-center font-bold text-3xl">No Blogs Found !</p>
+            </div>
+          ) : 
+          (
+          posts.map((post) => (
+            <BlogDetails key={post.id} post={post}/>
+          ))
+          )
+
+        )
+      }
     </div>
   );
 }
