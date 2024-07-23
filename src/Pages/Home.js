548eb57ef2fedd2493de21ce73e894ccbb74b1eb
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-07-23 20:47:24 +0530

    Updated Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,19 @@
-// import React from 'react'
-// import Blogs from '../components/Blogs';
-// import Header from '../components/Header';
-// import Pagination from '../components/Pagination';
+import React from 'react'
+import Blogs from '../components/Blogs';
+import Header from '../components/Header';
+import Pagination from '../components/Pagination';
 
 
-// const Home = () => {
-//   return (
-//     <div>
-//       <Header/>
-//       <div>
-//         <Blogs/>
-//         <Pagination/>
-//       </div>
-//     </div>
-//   )
-// }
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+        <Blogs/>
+        <Pagination/>
+      </div>
+    </div>
+  )
+}
 
-// export default Home;
+export default Home;
