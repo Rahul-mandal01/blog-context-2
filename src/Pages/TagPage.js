c6a9051e19f8f27108ec61eb08acee789cb9080f
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-07-26 16:33:10 +0530

    Update TagPage.js

--- src/Pages/TagPage.js
+++ src/Pages/TagPage.js
@@ -1,34 +1,34 @@
-// import React from 'react'
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import Blogs from '../components/Blogs';
-// import Header from '../components/Header';
-// import Pagination from '../components/Pagination';
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import Blogs from '../components/Blogs';
+import Header from '../components/Header';
+import Pagination from '../components/Pagination';
 
 
-// const TagPage = () => {
+const TagPage = () => {
 
-//   const navigation = useNavigate();
-//   const location = useLocation();
-//   const tag = location.pathname.split("/").at(-1);
+  const navigation = useNavigate();
+  const location = useLocation();
+  const tag = location.pathname.split("/").at(-1);
 
-//   return (
-//     <div>
-//         <Header/>
-//         <div>
-//           <button  onClick={() => navigation(-1)}>
-//             back
-//           </button>
+  return (
+    <div>
+        <Header/>
+        <div>
+          <button  onClick={() => navigation(-1)}>
+            back
+          </button>
 
-//           <h2>
-//             Blogs Tagged <span>#{tag}</span>
-//           </h2>
-//         </div>
+          <h2>
+            Blogs Tagged <span>#{tag}</span>
+          </h2>
+        </div>
 
-//         <Blogs/>
-//         <Pagination/>
+        <Blogs/>
+        <Pagination/>
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default TagPage;
+export default TagPage;
