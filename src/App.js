0af35d8224bc7ab9ffc025ba5af01fa12ef24610
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-07-30 20:30:20 +0530

    Updaate App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import "./App.css";
-import {Routes, Route,useSearchParams, useLocation} from "react-router-dom"
-import { useContext, useEffect } from "react";
-import { AppContext } from "./context/AppContext";
-import Home from "./Pages/Home";
-import CategoryPage from "./Pages/CategoryPage";
-import BlogPage from "./Pages/BlogPage";
-import TagPage from "./Pages/TagPage";
+// import "./App.css";
+// import {Routes, Route,useSearchParams, useLocation} from "react-router-dom"
+// import { useContext, useEffect } from "react";
+// import { AppContext } from "./context/AppContext";
+// import Home from "./Pages/Home";
+// import CategoryPage from "./Pages/CategoryPage";
+// import BlogPage from "./Pages/BlogPage";
+// import TagPage from "./Pages/TagPage";
 
 
 
 
-function App() {
-  const { fetchBlogPosts } = useContext(AppContext);
+// function App() {
+//   const { fetchBlogPosts } = useContext(AppContext);
 
-  const [searchParams, setSearchParams] = useSearchParams();
+//   const [searchParams, setSearchParams] = useSearchParams();
 
-  const location = useLocation();
+//   const location = useLocation();
 
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    const page = searchParams.get("page") ?? 1;
+//     const page = searchParams.get("page") ?? 1;
 
-    if(location.pathname.includes("tags")){
-      // iska matlab tag wala page show krna hai
-      const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
-      fetchBlogPosts(Number(page), tag);
+//     if(location.pathname.includes("tags")){
+//       // iska matlab tag wala page show krna hai
+//       const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
+//       fetchBlogPosts(Number(page), tag);
 
-    }
+//     }
 
-    else if(location.pathname.includes("categories")){
-      const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
-      fetchBlogPosts(Number(page), null , category);
-    }
+//     else if(location.pathname.includes("categories")){
+//       const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
+//       fetchBlogPosts(Number(page), null , category);
+//     }
 
-    else{
-      fetchBlogPosts(Number(page));
-    }
-    }, [location.pathname, location.search]);
+//     else{
+//       fetchBlogPosts(Number(page));
+//     }
+//     }, [location.pathname, location.search]);
 
-  return (
+//   return (
 
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/blog/:blogId" element={<BlogPage/>} />
-      <Route path="/tags/:tag" element={<TagPage/>} />
-      <Route path="/categories/:category" element={<CategoryPage/>} />
-    </Routes>
+//     <Routes>
+//       <Route path="/" element={<Home/>} />
+//       <Route path="/blog/:blogId" element={<BlogPage/>} />
+//       <Route path="/tags/:tag" element={<TagPage/>} />
+//       <Route path="/categories/:category" element={<CategoryPage/>} />
+//     </Routes>
 
-  );
-}
+//   );
+// }
 
-export default App;
+// export default App;
